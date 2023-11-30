ba0d1388d3dfdc4d99d7150b073ae083fb963fa8
jeffjr2060 <kevinchela714@gmail.com>
2023-11-30 08:32:57 +0300

    Second Commit

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
   res.send('Hello, Node.js!');
 });
 
+app.get('/register', (req, res) => {
+    res.send('Register Page');
+  });
+
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
